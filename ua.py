4184372a96641beb6c08db512afaff14c4253ca0
The jAkknn <ekaadifirmansyah1@gmail.com>
2025-01-25 21:35:08 +0700

    Update ua.py

--- ua.py
+++ ua.py
@@ -184,8 +184,8 @@ class Useragent:
     f'Barcelona {self.versi_istagram} Android ({self.device_android}; {self.dpi}; {self.pxl}; ASUS MOBILITY LIMITED/asus; {str(random.choice(self.asus))}; {str(random.choice(self.asus))}; qcom; in_ID; {self.kode})',
     f'Barcelona {self.versi_istagram} Android ({self.device_android}; {self.dpi}; {self.pxl}; OPPO MOBILITY LIMITED/oppo; {str(random.choice(self.oppo2))}; {str(random.choice(self.oppo))}; qcom; in_ID; {self.kode})',
     f'Barcelona {self.versi_istagram} Android ({self.device_android}; {self.dpi}; {self.pxl}; MICROMAX MOBILITY LIMITED/micromax; {str(random.choice(self.micromax))}; {str(random.choice(self.micromax))}; qcom; in_ID; {self.kode})',
-    f'Barcelona {self.versi_istagram} Android ({self.device_android}; {self.dpi}; {self.pxl}; ONEPLUS MOBILITY LIMITED/oneplus; {str(random.choice(self.oneplus))}; {str(random.choice(self.oneplus); qcom; in_ID; {self.kode})',
-    f'Barcelona {self.versi_istagram} Android ({self.device_android}; {self.dpi}; {self.pxl}; VIVO MOBILITY LIMITED/vivo; {str(random.choice(sel}; {str(random.choice(self.vivo))}; qcom; in_ID; {self.kode})',
+    f'Barcelona {self.versi_istagram} Android ({self.device_android}; {self.dpi}; {self.pxl}; ONEPLUS MOBILITY LIMITED/oneplus; {str(random.choice(self.oneplus))}; {str(random.choice(self.oneplus))}; qcom; in_ID; {self.kode})',
+    f'Barcelona {self.versi_istagram} Android ({self.device_android}; {self.dpi}; {self.pxl}; VIVO MOBILITY LIMITED/vivo; {str(random.choice(self.vivo2))}; {str(random.choice(self.vivo))}; qcom; in_ID; {self.kode})',
     f'Barcelona {self.versi_istagram} Android ({self.device_android}; {self.dpi}; {self.pxl}; POCO MOBILITY LIMITED/poco; {str(random.choice(self.poco))}; {str(random.choice(self.poco))}; qcom; in_ID; {self.kode})'
 ])
     def useragent_api(self):
